8728bcf8c8f1edb02c17327c105fca8c4d3ba32c
ih4temyself <dyakonenko123@gmail.com>
2024-11-25 17:46:39 +0200

    django cryptograhpy - outdated django lib

--- backend/botiki_exam/settings.py
+++ backend/botiki_exam/settings.py
@@ -8,7 +8,7 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env.development"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
@@ -29,7 +29,7 @@ CORS_ALLOWED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    "django_cryptography",
+    "encrypted_model_fields",
     "telegrambot",
     "mainpage",
     "authapp",
