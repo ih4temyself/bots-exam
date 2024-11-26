5d5fc118fa50148af3a558a5160e7d24c555e140
tottfiy <ivanko.lisenko@gmail.com>
2024-11-26 16:50:10 +0200

    Added bot token to env

--- backend/telegrambot/bot.py
+++ backend/telegrambot/bot.py
@@ -2,8 +2,8 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 
-TOKEN = '7700331589:AAF-V19Z0Q6YCZOxHnzwy-Ucmrf1RvDSYdY'
-NAME = '@catsfunnycleverbot'
+BOT_TOKEN = '7700331589:AAF-V19Z0Q6YCZOxHnzwy-Ucmrf1RvDSYdY'
+BOT_NAME = '@catsfunnycleverbot'
 
 # Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -30,8 +30,8 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'User ({update.message.chat.id} in {message_type}: "{text}")')
 
     if message_type == "group":
-        if NAME in text:
-            new_text: str = text.replace(NAME, '').strip()
+        if BOT_NAME in text:
+            new_text: str = text.replace(BOT_NAME, '').strip()
             response: str = handle_response(new_text)
         else:
             return
@@ -45,7 +45,7 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 if __name__ == '__main__':
     print('Starting bot...')
-    app = Application.builder().token(TOKEN).build()
+    app = Application.builder().token(BOT_TOKEN).build()
 
     # Commands
     app.add_handler(CommandHandler('start', start_command))
